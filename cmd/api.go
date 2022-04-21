59a942ba0e29d52ae3e628842a8e7d747a92e6b0
Runar Kristoffersen <gh@runar.dev>
2022-04-22 00:00:39 +0200

    fix: add more info to api-headers

--- cmd/api.go
+++ cmd/api.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/dustin/go-humanize"
 	"github.com/runar-rkmedia/go-common/logger"
+	"github.com/runar-rkmedia/go-common/utils"
 	"github.com/runar-rkmedia/skiver/handlers"
 	"github.com/runar-rkmedia/skiver/models"
 	"github.com/runar-rkmedia/skiver/types"
@@ -23,6 +24,7 @@ type Api struct {
 	cookies  []*http.Cookie
 	login    *types.LoginResponse
 	client   *http.Client
+	Headers  http.Header
 }
 
 func NewAPI(l logger.AppLogger, endpoint string) Api {
@@ -31,6 +33,7 @@ func NewAPI(l logger.AppLogger, endpoint string) Api {
 		l:        l,
 		endpoint: strings.TrimSuffix(endpoint, "/"),
 		client:   &c,
+		Headers:  http.Header{},
 	}
 	if l.HasDebug() {
 		l.Debug().Str("uri", endpoint).Msg("Using skiver-api")
@@ -54,32 +57,17 @@ func (a *Api) Login(username, password string) error {
 	if err != nil {
 		return fmt.Errorf("Failed to marshal login-payload: %w", err)
 	}
-	r, err := http.NewRequest(http.MethodPost, a.endpoint+"/api/login/", bytes.NewBuffer(b))
+	r, err := a.NewRequest(http.MethodPost, "/api/login/", bytes.NewBuffer(b))
 	if err != nil {
 		return fmt.Errorf("failed to create login-request: %w", err)
 	}
-	r.Header.Add("Content-Type", "application/json")
 
 	res, err := http.DefaultClient.Do(r)
 	if err != nil {
 		return fmt.Errorf("login-request failed: %w", err)
 	}
-	defer res.Body.Close()
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return fmt.Errorf("failed reading body of login-request: %w", err)
-	}
-	if res.StatusCode >= 300 {
-		var j models.APIError
-		err = json.Unmarshal(body, &j)
-		if err == nil {
-			return fmt.Errorf("Login-request return a %d-response: %s (%s) %#v", res.StatusCode, j.Error.Message, j.Error.Code, j.Details)
-		}
-
-		return fmt.Errorf("Login-request return a %d-response: %s", res.StatusCode, string(body))
-	}
 	var j types.LoginResponse
-	err = json.Unmarshal(body, &j)
+	res, err = a.Do(r, &j)
 	if err != nil {
 		return fmt.Errorf("failed reading body of login-request: %w", err)
 	}
@@ -95,39 +83,18 @@ func (a *Api) Login(username, password string) error {
 	return nil
 
 }
+
 func (a Api) Import(projectName string, kind string, locale string, reader io.Reader) error {
 	if len(a.cookies) == 0 {
 		return fmt.Errorf("Not logged in")
 	}
-	r, err := http.NewRequest(http.MethodPost, a.endpoint+"/api/import/"+kind+"/"+projectName+"/"+locale, reader)
+	r, err := a.NewRequest(http.MethodPost, "/api/import/"+kind+"/"+projectName+"/"+locale, reader)
 	if err != nil {
 		return fmt.Errorf("failed to create import-request: %w", err)
 	}
-	r.Header.Add("Content-Type", "application/json")
-	for _, c := range a.cookies {
-		r.AddCookie(c)
-	}
 
-	res, err := http.DefaultClient.Do(r)
-	if err != nil {
-		return fmt.Errorf("import-request failed: %w", err)
-	}
-	defer res.Body.Close()
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return fmt.Errorf("failed reading body of import-request: %w", err)
-	}
-	if res.StatusCode >= 300 {
-		var j models.APIError
-		err = json.Unmarshal(body, &j)
-		if err == nil {
-			return fmt.Errorf("import-request returned %d-response: %s (%s) %#v", res.StatusCode, j.Error.Message, j.Error.Code, j.Details)
-		}
-
-		return fmt.Errorf("import-request returned %d-response: %s", res.StatusCode, string(body))
-	}
 	var j handlers.ImportResult
-	err = json.Unmarshal(body, &j)
+	res, err := a.Do(r, &j)
 	if err != nil {
 		return fmt.Errorf("failed reading body of import-request: %w", err)
 	}
@@ -151,7 +118,7 @@ func (a Api) Export(projectName string, format string, locale string, writer io.
 	if len(a.cookies) == 0 {
 		return fmt.Errorf("Not logged in")
 	}
-	r, err := http.NewRequest(http.MethodGet, a.endpoint+"/api/export/", nil)
+	r, err := a.NewRequest(http.MethodGet, "/api/export/", nil)
 	if err != nil {
 		return fmt.Errorf("failed to create export-request: %w", err)
 	}
@@ -160,30 +127,13 @@ func (a Api) Export(projectName string, format string, locale string, writer io.
 	q.Set("locale", locale)
 	q.Set("project", projectName)
 	r.URL.RawQuery = q.Encode()
-	r.Header.Add("Content-Type", "application/json")
-	for _, c := range a.cookies {
-		r.AddCookie(c)
-	}
 
-	res, err := http.DefaultClient.Do(r)
+	res, err := a.Do(r, nil)
 	if err != nil {
 		return fmt.Errorf("export-request failed: %w", err)
 	}
 	defer res.Body.Close()
 
-	if res.StatusCode >= 300 {
-		body, err := ioutil.ReadAll(res.Body)
-		if err != nil {
-			return fmt.Errorf("failed reading body of export-request: %w", err)
-		}
-		var j models.APIError
-		err = json.Unmarshal(body, &j)
-		if err == nil {
-			return fmt.Errorf("export-request return a %d-response: %s (%s) %#v", res.StatusCode, j.Error.Message, j.Error.Code, j.Details)
-		}
-
-		return fmt.Errorf("export-request return a %d-response: %s", res.StatusCode, string(body))
-	}
 	written, err := io.Copy(writer, res.Body)
 	if a.l.HasDebug() {
 		a.l.Debug().
@@ -196,4 +146,85 @@ func (a Api) Export(projectName string, format string, locale string, writer io.
 	}
 	return nil
 
+}
+
+// NewRequest is a thin wrapper around http.NewRequest
+func (a *Api) NewRequest(method string, subpath string, body io.Reader) (*http.Request, error) {
+	uri := a.endpoint + subpath
+	if a.l.HasDebug() {
+		a.l.Debug().
+			Str("method", subpath).
+			Str("subpath", subpath).
+			Str("uri", uri).
+			Msg("Creating request")
+	}
+	r, err := http.NewRequest(method, uri, body)
+	if err != nil {
+		return r, err
+	}
+	r.Header = a.Headers.Clone()
+	r.Header.Set("Content-Type", "application/json")
+	for _, c := range a.cookies {
+		r.AddCookie(c)
+	}
+	return r, err
+}
+
+// Do a http.request. If j is not nil, it will unmarshal to that destination
+func (a Api) Do(r *http.Request, j interface{}) (*http.Response, error) {
+	reqId := r.Header.Get("X-Request-ID")
+	if reqId == "" {
+		id, err := utils.ForceCreateUniqueId()
+		if err != nil {
+			a.l.Warn().Err(err).Str("unique-id", id).Msg("an error occured when attempting to create a unique id for the request.")
+		}
+		reqId = id
+		r.Header.Set("X-Request-ID", id)
+	}
+	if a.l.HasDebug() {
+		a.l.Debug().
+			Str("method", r.Method).
+			Str("request-id", reqId).
+			Interface("headers", r.Header).
+			Str("uri", r.URL.String()).
+			Msg("Doing request")
+	}
+	res, err := a.client.Do(r)
+	if err != nil {
+		return res, err
+	}
+	if a.l.HasDebug() {
+		a.l.Debug().
+			Str("method", r.Method).
+			Str("request-id", reqId).
+			Str("uri", r.URL.String()).
+			Interface("headers", res.Header).
+			Interface("status-code", res.StatusCode).
+			Msg("Result of request")
+	}
+	if res.StatusCode >= 300 {
+		defer res.Body.Close()
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return res, fmt.Errorf("failed reading body of request: %w", err)
+		}
+		var j models.APIError
+		err = json.Unmarshal(body, &j)
+		if err == nil {
+			return res, fmt.Errorf("request returned %d-response: %s (%s) %#v", res.StatusCode, j.Error.Message, j.Error.Code, j.Details)
+		}
+		return res, fmt.Errorf("request returned %d-response: %s", res.StatusCode, string(body))
+	}
+	if j != nil {
+		defer res.Body.Close()
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return res, fmt.Errorf("failed reading body of request: %w", err)
+		}
+		err = json.Unmarshal(body, &j)
+		if err != nil {
+			return res, err
+		}
+	}
+	return res, err
 }
